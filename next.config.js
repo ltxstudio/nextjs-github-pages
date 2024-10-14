a5de6b59de3d0e5d7c5aba05608d7e0c5ca1a24d
ltxstudio <ltxstudio.info@gmail.com>
2024-10-15 05:32:29 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/gh-pages-test",
+  basePath: "/nextjs-github-pages",
 };
 
 module.exports = nextConfig;
