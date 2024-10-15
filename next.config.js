768311de5eed7a7acc97caac9d000e6f91cee92a
ltxstudio <ltxstudio.info@gmail.com>
2024-10-15 06:13:04 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
+const dotenv = require('dotenv');
+dotenv.config();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   basePath: "/nextjs-github-pages",
 };
 
-module.exports = nextConfig;
+module.exports = {
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    APIRONE_API_KEY: process.env.APIRONE_API_KEY,
+  },
+};
